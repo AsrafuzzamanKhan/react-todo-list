28512d0728ba953015b590d0f391263af6331e07
AsrafuzzamanKhan <asrafuzzamankhan89@gmail.com>
2024-02-09 04:24:21 +0600

    modify designe

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
     return (
-        <div className="w-full fixed top-0 text-center z-[9999]  bg-zinc-800 py-4 text-white text-2xl font-semibold ">
-            React Todo List
+        <div className="w-full fixed top-0 text-center z-[9999]  bg-black py-6 text-white text-2xl font-semibold ">
+            Todo List App
         </div>
     )
 }
 
-export default Header
+export default Header;
